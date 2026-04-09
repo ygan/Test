509dbb9956474aad710f9eff3f50b5a20c70e004
ygan <46568175+ygan@users.noreply.github.com>
2026-04-09 17:32:55 +0100

    Update ssl.py

--- ssl.py
+++ ssl.py
@@ -1,3 +1,12 @@
-import ssl
-print(ssl.OPENSSL_VERSION)
-print(ssl.get_default_verify_paths())
+import sys, glob
+try:
+    import certifi
+    print(certifi.where())
+except Exception:
+    cands = []
+    for p in glob.glob(sys.prefix + "/**/*.pem", recursive=True):
+        s = p.lower()
+        if s.endswith("cacert.pem") or "certifi" in s or "ca-bundle" in s or "bundle" in s:
+            cands.append(p)
+    if cands:
+        print(cands[0])
